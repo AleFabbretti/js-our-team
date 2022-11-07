bf8aa65bbc7bd62502e53abb6b53c9fbe794be53
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-11-07 15:11:50 +0100

    Aggiunti commenti

---
+++ js/main.js
@@ -0,0 +1,11 @@
+`use strict`
+
+//Creare l’array di oggetti con le informazioni fornite.
+
+//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto.
+
+//Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+//BONUS 1:Trasformare la stringa foto in una immagine effettiva
+
+//BONUS 2:Organizzare i singoli membri in card/schede.
