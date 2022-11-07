6585de60e54313fca2ce9415bf8056aa42eb9a05
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-11-07 15:56:15 +0100

    Aggiunto array con informazioni

--- js/main.js
+++ js/main.js
@@ -2,6 +2,40 @@
 
 //Creare l’array di oggetti con le informazioni fornite.
 
+const members = [
+    {
+        'NameMember' : "Wayne Barnett",
+        'role' : 'Founder & CEO',
+        'imgSource': 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        'NameMember' : "Angela Caroll",
+        'role' : 'Chief Editor',
+        'imgSource': 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        'NameMember' : "Walter Gordon",
+        'role' : 'Office Manager',
+        'imgSource': 'walter-gordon-office-manager.jpg'
+    },
+    {
+        'NameMember' : "Angela Lopez",
+        'role' : 'Social Media Manager',
+        'imgSource': 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        'NameMember' : "Scott Estrada",
+        'role' : 'Developer',
+        'imgSource': 'scott-estrada-developer.jpg'
+    },
+    {
+        'NameMember' : "Barbara Ramos",
+        'role' : 'Graphic Designer',
+        'imgSource': 'barbara-ramos-graphic-designer.jpg'
+    }
+    
+];
+
 //Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto.
 
 //Stampare le stesse informazioni su DOM sottoforma di stringhe
